945688038fc72a24470f52de41292a228baafebe
Byron Smith <bsmith89@gmail.com>
2014-11-13 10:25:28 -0500

    Initialization squashes all previous commits.

--- init_project.sh
+++ init_project.sh
@@ -8,5 +8,6 @@ git config --local filter.dropoutput_ipynb.smudge cat
 unlink README.md
 ln -s NOTE.md README.md
 rm -i $0
+git reset --soft $(git rev-list --max-parents=0 HEAD)
 git add -A
-git commit -em "Clean project.  Let's get started!"
+git commit --amend -em "Clean project.  Let's get started!"
