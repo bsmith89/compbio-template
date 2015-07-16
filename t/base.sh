5c543788f684d6e5801a88e25c977b686ebfc0c3
Byron Smith <bsmith89@gmail.com>
2015-07-16 19:25:08 -0400

    Add testing script.

--- t/base.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Set flags and variables to make an effective test script.
-set -o nounset -o errexit -o pipefail
---
+++ t/run_tests.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Run all test scripts found recursively below $1
+# Place a semaphore during testing
+set -o nounset -o errexit -o pipefail
+
+LOCKDIR=.testing
+mkdir -p dirname "$LOCKDIR"
+if ! mkdir "$LOCKDIR"; then
+    echo "Cannot acquire lock. '$LOCKDIR' exists.  Exiting." >&2
+    exit 1
+fi
+
+[ -z "$1" ] && TEST_DIR="$1" || TEST_DIR='.'
+START_DIR=$(pwd)
+export PREFIX=build
+# Save the original branchname so we can revert
+START_BRANCH=`git rev-parse --abbrev-ref HEAD`
+# Switch to a random branchname (not quite threadsafe) so we don't mess up
+# history
+TEST_BRANCH=$(mktemp -u _test_XXXXXXX)
+# Commit everything in the working directory
+git stash save --include-untracked
+git checkout -B "$TEST_BRANCH"
+git stash apply
+git add -A
+git commit --allow-empty -m "[TEST COMMIT; TO BE REMOVED]"
+mkdir -p "$PREFIX"
+ALL_TESTS=$(find "$TEST_DIR" -name test_*)
+
+teardown() {
+    cd "$START_DIR"
+    git checkout "$START_BRANCH"
+    git stash pop
+    git branch -D "$TEST_BRANCH"
+    rm -r "$LOCKDIR"
+}
+
+trap teardown EXIT
+
+for t in "$ALL_TESTS"; do
+    echo "$t"
+    bash "$t"
+done
