c15ba71202484bf4b4c110e1807cc2f9827c6dfd
Byron Smith <bsmith89@gmail.com>
2015-07-16 20:33:47 -0400

    Fix bugs in testing script.

--- t/run_tests.sh
+++ t/run_tests.sh
@@ -3,29 +3,38 @@
 # Place a semaphore during testing
 set -o nounset -o errexit -o pipefail
 
-LOCKDIR=.testing
-mkdir -p dirname "$LOCKDIR"
-if ! mkdir "$LOCKDIR"; then
-    echo "Cannot acquire lock. '$LOCKDIR' exists.  Exiting." >&2
-    exit 1
+if [ -z "$1" ]; then
+    TEST_SCRIPT_DIR="$1"
+else
+    TEST_SCRIPT_DIR='.'
 fi
 
-[ -z "$1" ] && TEST_SCRIPT_DIR="$1" || TEST_SCRIPT_DIR='.'
-export TEST_START_DIR=$(pwd)
-export TEST_PREFIX=$(realpath build)
-# Save the original branchname so we can revert
-START_BRANCH=`git rev-parse --abbrev-ref HEAD`
-# Switch to a random branchname (not quite threadsafe) so we don't mess up
-# history
-TEST_BRANCH=$(mktemp -u _test_XXXXXXX)
-# Commit everything in the working directory
-git stash save --include-untracked
-git checkout -B "$TEST_BRANCH"
-git stash apply
-git add -A
-git commit --allow-empty -m "[TEST COMMIT; TO BE REMOVED]"
-mkdir -p "$TEST_PREFIX"
-ALL_TESTS=$(find "$TEST_SCRIPT_DIR" -name test_*)
+setup() {
+    LOCKDIR=.testing
+    mkdir -p $(dirname "$LOCKDIR")
+    if ! mkdir "$LOCKDIR"; then
+        echo "Cannot acquire lock. '$LOCKDIR' exists.  Exiting." >&2
+        exit 1
+    fi
+
+    export TEST_START_DIR=$(pwd)
+    export TEST_PREFIX=$(realpath build)
+    # Save the original branchname so we can revert
+    START_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    # Switch to a random branchname (not quite threadsafe) so we don't mess up
+    # history
+    TEST_BRANCH=$(mktemp -u _test_XXXXXXX)
+    # Commit everything in the working directory
+    git stash save
+    git checkout -B "$TEST_BRANCH"
+    git stash apply
+    git add -A
+    git commit --allow-empty -m "[TEST COMMIT; TO BE REMOVED]"
+    mkdir -p "$TEST_PREFIX"
+    ALL_TESTS=$(find "$TEST_SCRIPT_DIR" -name test_*)
+}
+
+setup
 
 teardown() {
     cd "$TEST_START_DIR"
@@ -37,7 +46,6 @@ teardown() {
 
 trap teardown EXIT
 
-for t in "$ALL_TESTS"; do
-    echo "$t"
+for t in $ALL_TESTS; do
     bash "$t"
 done
