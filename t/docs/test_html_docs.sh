7f5cd0446e625edc235e9452aea760e1dd396e11
Byron Smith <bsmith89@gmail.com>
2015-07-17 13:13:37 -0400

    Test doc generation.

--- t/docs/test_html_docs.sh
+++ t/docs/test_html_docs.sh
@@ -1,1 +1,13 @@
-make --what-if=NOTE.md NOTE.html
+setup() {
+    if make --question NOTE.html; then
+        make NOTE.html
+    fi
+    BIB="$TEST_OUT_DIR"/external.bib
+    touch "$BIB"
+}
+
+setup
+
+make --question NOTE.html
+! make --question --what-if=NOTE.md NOTE.html
+! make --question --what-if="$BIB" EXTERNAL_BIBS="$BIB" NOTE.html
